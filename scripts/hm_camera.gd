43c671e8189789391edfd2d78f0ef89faa5807d2
Dehedero <43905800+Dehedero@users.noreply.github.com>
2026-05-01 15:28:51 +0300

    camera +  bg

---
+++ scripts/hm_camera.gd
@@ -0,0 +1,19 @@
+extends Camera2D
+class_name HmCamera
+
+@export var left_limit : int
+@export var right_limit : int
+@export var follow_speed : float =  5.0
+
+@export var target : Node2D
+
+func _ready() -> void:
+	position_smoothing_enabled = false
+
+func _physics_process(delta: float) -> void:
+	var target_pos : Vector2 = global_position
+	target_pos.x = target.global_position.x
+	target_pos = target_pos.round()
+	var weight : float = 1.0 - exp(-follow_speed * delta)
+	global_position = global_position.lerp(target_pos, weight)
+	global_position.x = clampf(global_position.x, left_limit, right_limit)
